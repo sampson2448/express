9a4df2ffcaf8deb6e0ec0e98d17f3b5c7f0c7089
sampson2448 <sampson2448@yahoo.com>
2024-01-09 09:01:14 -0500

    first commit

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Students A ', done: true, _id: 1},
+  {text: 'Students B', done: true, _id: 2},
+  {text: 'Students C', done: false, _id: 3},
+]
+
+export { 
+	students
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+// import modules
+
+import express from 'express'
+import { students } from './data/students-data.js'
+
+// create Express app
+
+const app = express()
+
+// configure the app (app.set)
+app.set('view engine', 'ejs')
+// mount Middleware (app.use)
+
+
+
+// Mount routes
+app.get('/', function(req, res) {
+  res.redirect('/students')
+})
+
+app.get('/', function(req, res) {
+  res.send('<h1>hello, friend</h1>')
+})
+
+app.get('/home', function(req, res) {
+  res.render('home')
+
+})
+
+app.get('/students', function(req, res) {
+  res.render('students/index', {
+    students: students
+  })
+})
+
+// tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
